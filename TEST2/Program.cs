b05398029db6f145b613366e1030a7135453b697
Dimityr <dzambazov1986@gmail.com>
2024-02-26 22:48:29 +0200

    Add project files.

---
+++ TEST2/Program.cs
@@ -0,0 +1,25 @@
+﻿static string FoldArray(int[] arr)
+{
+    int k = arr.Length / 4;
+
+    int[] topRow = arr
+        .Take(k)
+        .Reverse()
+        .Concat(arr.Skip(arr.Length - k).Reverse())
+        .ToArray();
+
+    int[] bottomRow = arr
+        .Skip(k)
+        .Take(k * 2)
+        .ToArray();
+
+    string result = string.Empty;
+    for (int i = 0; i < topRow.Length; i++)
+    {
+        result += $"{topRow[i] + bottomRow[i]} ";
+    }
+
+    return result.Trim();
+}
+Console.WriteLine(FoldArray(new int[] {1, 2, 3, 4, 5, 6, 7, 8}));
+
